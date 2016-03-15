afcf79ad909f714514c9598fb310dcb7f9303359
Anshumaan Bajpai <Anshumaan Bajpai>
2016-03-15 18:39:53 -0400

    test

--- ds_utils.py
+++ ds_utils.py
@@ -4,11 +4,20 @@ Created on Sun Feb 28 19:45:01 2016
 
 @author: Anshumaan
 """
+# Importing the required library
+import pandas as pd
+import os
+import tarfile
+import os
+import numpy as np
+import cv2
+import sys
 
 # The script contains all the utility functions that I use for typical data
 # analysis for the kaggle projects. The functions are related to extracting
 # compressed files, performing data cleaning and so on
 
+###############################################################################
 def uncom_tgz(fpath):
     '''
     Function to uncompress a .tgz file
@@ -18,10 +27,6 @@ def uncom_tgz(fpath):
 
     return: None
     '''
-
-    # Importing the required library
-    import tarfile
-    import os
 
     # Uncompressing the files in the same directory as the compressed file
     # Obtaining the directory based on file name
@@ -39,7 +44,7 @@ def uncom_tgz(fpath):
 
 #uncom_tgz('MassiveData/yelpDS/sample_submission.csv.tgz')
 
-
+###############################################################################
 def csv_overview(fpath):
     '''
     Function to analyze a basic csv file to give an overview
@@ -51,10 +56,6 @@ def csv_overview(fpath):
     'size in kb': Size of the file in kbs
     'readfile': A pandas data frame with basic file info
     '''
-
-    # Importing the required library
-    import pandas as pd
-    import os
 
     # Obtaining the directory and the filename
     fdir, sep, fname = fpath.rpartition('/')
@@ -72,3 +73,25 @@ def csv_overview(fpath):
     return output
 
 #csv_overview('MassiveData/yelpDS/train.csv')
+
+
+###############################################################################
+## A class that loads an image as a RGB object
+class RGBHistogram:
+    # Constructor
+    def __init__(self, bins):
+        # store the number of bins the histogram will use
+        self.bins=bins
+
+    # function to create feature vectors
+    def describe(self, image):
+        # Compute a 3D histogram in RGB colorspace
+        hist = cv2.calcHist([image], [0,1,1], None, self.bins,
+                            [0, 256, 0,256, 0,256])
+        # Normalizing the histograms so that images to compare different sizes
+        #print type(hist)
+        cv2.normalize(hist, hist)
+
+        # Return the 3D histogram as a flattened array
+        return hist.flatten()
+
